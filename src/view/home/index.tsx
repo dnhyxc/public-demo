cbc9a2916c7cc0dde81369de67ec910044212c86
dnhyxc <2640559435@qq.com>
2025-04-25 14:11:44 +0800

    feat: init

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import { Button } from 'antd';
 import styles from './index.less';
 
 const Home = () => {
+  const [status, setStatus] = useState<boolean>(true);
+
+  const onClick = () => {
+    setStatus(!status);
+  };
+
   return (
     <div className={styles.container}>
       <h1>hello dnhyxc!!!</h1>
 
-      <Button type="primary">test</Button>
+      {status ? (
+        <h3>你好！我有一个帽衫，向左向右向前看～～～</h3>
+      ) : (
+        <h3>
+          风，是大自然的精灵，时而温柔地抚过脸庞，时而狂野地舞动树梢。它带着远方的故事，穿梭在城市的街道，掠过寂静的山谷，抚慰着每一个孤独的灵魂。
+        </h3>
+      )}
+
+      <Button type="primary" onClick={onClick}>
+        test
+      </Button>
     </div>
   );
 };
