7bc3e7d53c5d1b93789af0e592ee493523510a8b
wyh741953737 <741953737@qq.com>
2025-06-06 17:27:29 +0800

    测试

---
+++ src/view/home/guide.tsx
@@ -0,0 +1,151 @@
+import { FC, useCallback, useEffect, useLayoutEffect, useRef } from 'react';
+import style from './guide.less';
+import { Button } from 'antd-mobile';
+import Joyride, { Step}  from 'react-joyride';
+import GuideStep from './guideStep.jpg'
+import GuideStepDown from './guideStepDown.jpg'
+import GuidBg from './guideBg.jpg'
+
+
+interface PropTypes {
+    run: boolean;
+    steps: Step[],
+    skipCb: ()=>void
+}
+const Guide:FC<PropTypes> = (props) => {
+    const { run, steps, skipCb } = props
+    const stepOneNormalContent =  [ // 普通员工
+            { title: '数字人力美眉', subTitle: 'AI赋能，全方位解决人事场景问题' },
+            { title: '高频功能定制', subTitle: '自定义常用功能，高频操作快捷触达' },
+            { title: '流程中心优化', subTitle: '直观高效，切换便捷' },
+            {  title: '功能触达便捷', subTitle: '分类清晰，容易查找' },
+            {  title: '个人中心拆分', subTitle: '根据移动端使用习惯优化入口' },
+    ]
+    const renderContent = () => {
+        return (
+            <div className={style.stepOne}>
+                <div className={style.title}>欢迎进入新版人力门户</div>
+                <div className={style.subTitle}>全新数字化办公体验</div>
+                {stepOneNormalContent.map(item => (
+                    <div className={style.itemWrap} key={item.title}>
+                        <span>你好</span>
+                        <div className={style.itemRight}>
+                            <div className={style.itemTop}>{item.title}</div>
+                            <div className={style.itemBottom}>{item.subTitle}</div>
+                            </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+    const renderSubTitle = useCallback((content: string) => {
+        return content.includes('<br/>') ? content.split('<br/>').map(item => <div>{item}</div>) : content
+    }, [])
+    // const [background, setBackground] = useState(GuideStep)
+    const boxRef= useRef<any>(null)
+    const cardRef= useRef<any>(null)
+
+    useLayoutEffect(() => {
+        const handleScroll = () => {
+            // console.log(parseInt(cardRef.current?.top),parseInt(boxRef.current?.top))
+            if(run) {
+                const card = document.querySelector('.__floater__open')?.getBoundingClientRect()      
+                const box = document.querySelector('.react-joyride__spotlight')?.getBoundingClientRect()
+                if(card && box) {
+                    // console.log(222, card.top, box.top,  card.top > box.top)
+                    // setBackground(card.top < box.top ? GuideStepDown : GuideStep)
+                } 
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+          window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+    const polygon = document.querySelector('.__floater__arrow polygon');
+    const changeArrowSize = (polygon: any) => {
+        if (polygon) {
+            const points = polygon.getAttribute('points')
+            console.log('----', points)
+        }
+    }
+    useEffect(() => {
+        changeArrowSize(polygon);
+        const onScroll = () => {
+            changeArrowSize(polygon);
+        }; // 使用防抖处理滚动事件
+        window.addEventListener('scroll', onScroll);
+    }, [polygon])
+    // 先执行一次，防止箭头大小为设置成功
+    const tooltipComponent = ({
+        index,
+        step,
+        isLastStep,
+        size,
+        primaryProps,
+        skipProps,
+        tooltipProps,
+    }) => {
+        isLastStep && showArrow()
+        cardRef.current =  document.querySelector('.__floater__open')?.getBoundingClientRect()      
+        boxRef.current = document.querySelector('.react-joyride__spotlight')?.getBoundingClientRect()
+        let image = GuideStep
+        if(cardRef.current && boxRef.current) {
+            // console.log(parseInt(cardRef.current.top),parseInt(boxRef.current.top))
+            if(cardRef.current.top < boxRef.current.top) {
+                image = GuideStepDown
+            }
+        } 
+        return (
+            <div
+                {...tooltipProps}
+                className={[style.guideWrap, index=== 0 ? '' : style.otherStep].join(' ')}
+                style={{ backgroundImage: `url(${index === 0 ? GuidBg : image})`}}
+            >
+                {index === 0 ? renderContent() : 
+            <div className={style.stepWrap}>
+                <div className={style.stepTitle}>{step.title}</div>
+                <div className={style.stepSubTitle}>{renderSubTitle(step.content)}</div>                
+                </div>}
+                <div className={style.toolbox}>
+                    <div className={style.toolStep}>
+                        {index + 1}/{size}
+                    </div>
+                    <div className={style.toolRight}>
+                        <div className={style.toolSkip} onClick={skipCb}>
+                            {isLastStep ? '知道了' : '跳过'}
+                        </div>
+                        <Button className={style.toolNext} {...primaryProps}>
+                            {isLastStep ? '开启AI全新体验' : '下一步'}
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <Joyride
+            steps={steps}
+            // 这里如果需要记录用户是否已经完成过引导，就自己根据后端记录的用户完成引导状态手动控制 run 为 false，这样该用户下次进来就不会再显示引导了
+            run={run}
+            tooltipComponent={tooltipComponent}
+            continuous // 允许连续步骤
+            showProgress // 显示进度
+            showSkipButton // 显示跳过按钮
+            disableOverlayClose
+            locale={{ skip: '跳过' }}
+            spotlightPadding={4}
+            // disableScrolling
+            scrollOffset={100}
+            styles={{
+                options: {
+                    arrowColor: '#E03A53',
+                    primaryColor: '#ff6b6b', // 自定义主色调
+                    zIndex: 1000, // 确保在最上层
+                },
+            }}
+        />
+    );
+};
+
+export default Guide;
--- src/view/home/index.jsx
+++ src/view/home/index.jsx
@@ -1,146 +1,66 @@
 import { useState, useRef, useEffect, useCallback } from 'react';
 import './index.css';
-import { Toast, Modal, Popup, Button } from 'antd-mobile';
-import { flushSync } from 'react-dom';
-import Joyride from 'react-joyride';
-// import Guide from './guide';
-// import TestImg from './bg3.jpg';
-// import './guide.css';
-
+import Guide from './guide'
 const WeChatVoiceRecorder = () => {
-  const joyrideRef = useRef(null);
-  const [stepIndex, setStepIndex] = useState(0);
-  const [curIndex, setCurIndex] = useState(0);
-
-  const handleSkip = useCallback(() => {
-    // setRun(false)
-  }, []);
-
-  const renderContent = () => {
-    return (
-      <div>
-        <h1 style={{ color: '#000' }}>高频功能定制</h1>
-        <h2 style={{ color: '#000' }}>添加常用功能，i定义</h2>
-      </div>
-    );
-  };
-
-  const changeArrowSize = (polygon) => {
-    if (polygon) {
-      // 获取 points 属性值并替换 32 为 20，16 为 10
-      const points = polygon
-        .getAttribute('points')
-        .replace(/32/g, '20')
-        .replace(/16/g, '10');
-      polygon.setAttribute('points', points);
-    }
-  };
-
-  const tooltipComponent = ({
-    index,
-    step,
-    isLastStep,
-    size,
-    primaryProps,
-    skipProps,
-    tooltipProps,
-  }) => {
-    console.log(
-      '--',
-      index,
-      step.placement.startsWith('top'),
-      step.placement.startsWith('bottom')
-    );
-
-    const polygon = document.querySelector('.__floater__arrow polygon');
-
-    // 先执行一次，防止箭头大小为设置成功
-    changeArrowSize(polygon);
-
-    useEffect(() => {
-      const onScroll = () => {
-        changeArrowSize(polygon);
-      };
-      // 使用防抖处理滚动事件
-      window.addEventListener('scroll', onScroll);
-
-      return () => {
-        window.removeEventListener('scroll', onScroll);
-      };
-    }, [step.placement, polygon]);
-
-    return (
-      <div
-        {...tooltipProps}
-        style={{
-          width: '280px',
-          height: 'auto',
-          padding: '12px 20px',
-          // backgroundImage: `url(${TestImg})`,
-          background: '#E03A53',
-          backgroundSize: 'contain',
-          borderRadius: '10px',
-          position: 'relative',
-        }}
-      >
-        {renderContent()}
-        {/* <div
-          style={{
-            position: 'absolute',
-            [step.placement.startsWith('top') ? 'bottom' : 'top']: '-10px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            width: 0,
-            height: 0,
-            // borderLeft: '10px solid transparent',
-            // borderRight: '10px solid transparent',
-            // borderTop: step.placement.startsWith('top')
-            //   ? '10px solid #E03A53'
-            //   : '10px solid transparent',
-            // borderBottom: step.placement.startsWith('bottom')
-            //   ? '10px solid #E03A53'
-            //   : '10px olid transparent',
-          }}
-        /> */}
-        <div className="toolbox">
-          <div className="toolStep">
-            {index + 1}/{size}
-          </div>
-          <div className="toolRight">
-            <div className="toolSkip" onClick={handleSkip}>
-              {isLastStep ? '知道了' : '跳过'}
-            </div>
-            <Button className="toolNext" {...primaryProps}>
-              {isLastStep ? '开启AI全新体验' : '下一步'}
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  };
 
   const steps = [
     {
       target: '.feature-1',
-      placement: 'bottom',
+      placement: 'center',
+      title: '',
       disableBeacon: true, // 这里是开启自动引导的关键属性
+      content: '',
     },
     {
       target: '.feature-2',
-      placement: 'bottom',
+      // placement: 'bottom',
+      title: '高频功能定制',
+      content: '增加常用功能，可自定义配置，高频操作快捷触达',
     },
     {
       target: '.feature-3',
-      placement: 'bottom',
+      // placement: 'top',
+      title: '流程中心优化',
+      content: '申请/待办入口首页直观呈现，帮助您快速了解已申请流程进展',
+    },
+    {
+      target: '.feature-4',
+      // placement: 'top',
+      title: '功能触达便捷',
+      content: '功能和专业系统在这里而且分组啦～更易查找',
+    },
+    {
+      target: '.feature-5',
+      // placement: 'top',
+      title: '个人中心拆分',
+      content: '根据移动端使用习惯优化入口，拆分为独立菜单',
+    },
+    {
+      target: '.feature-6',
+      // placement: 'bottom',
+      title: '数字人力美眉',
+      content: '首页入口点击，或在所有页面下拉均可唤起我',
     },
   ];
+  useEffect(() => {
+    setRun(true)
+  }, [])
 
+  const [run ,setRun] = useState(false)
   return (
     <div id="mainApp" style={{ height: '100vh', background: '#fff' }}>
+      {run && <Guide steps={steps} run={run} skipCb={() => setRun(false)} />}
       <div className="feature feature-1">功能区域 1</div>
       <div className="feature feature-2">功能区域 2</div>
       <div className="feature feature-3">功能区域 3</div>
-      <Joyride
+      <div className="feature feature-4">功能区域 4</div>
+      <div className="feature feature-5">功能区域 5</div>
+      <div className="feature feature-6">功能区域 6</div>
+
+
+
+
+      {/* <Joyride
         steps={steps}
         // 这里如果需要记录用户是否已经完成过引导，就自己根据后端记录的用户完成引导状态手动控制 run 为 false，这样该用户下次进来就不会再显示引导了
         run
@@ -155,7 +75,7 @@ const WeChatVoiceRecorder = () => {
             zIndex: 1000, // 确保在最上层
           },
         }}
-      />
+      /> */}
 
       <div
         className="chatFooters"
